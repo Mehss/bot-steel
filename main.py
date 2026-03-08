0bda1c6c7130a8419abfd6080e32e344251ff607
Mehss <edgar.adirga@gmail.com>
2026-03-08 10:08:08 +0700

    calendars

--- main.py
+++ main.py
@@ -1253,7 +1253,7 @@ times = [
 
 
 def get_calendar_name() -> str:
-    start_date = datetime.datetime(2025, 8, 31, 0, 0, 0, tzinfo=jkt)
+    start_date = datetime.datetime(2026, 4, 4, 0, 0, 0, tzinfo=jkt)
     now = datetime.datetime.now(jkt)
 
     delta_days = (now - start_date).days + 1
@@ -1266,7 +1266,7 @@ def get_calendar_name() -> str:
     return calendar_name
 
 async def update_calendar():
-    channel_calendar = bot.get_channel(1396001668633722931)
+    channel_calendar = bot.get_channel(1443642761164095600)
     channel_name = f"📅 {get_calendar_name()}"
     print(channel_name)
     try:
@@ -1338,32 +1338,43 @@ async def daily_task_run():
 
 def get_in_game_date(irl_day_number):
     months = [
-        "🌸Terra", "🌸Marzen", "🌞Saturna", "🌞Venira",
-        "🍂Juvion", "🍂Mercion", "🧊Urantis", "🧊Nevaris"
+        "🌻 文月 Futsuki", "🌻 葉月 Hazuki", "🌻 長月 Nagatsuki", "🍁 神無月 Kannazuki", "🍁 霜月 Shimotsuki",
+        "🍁 師走 Shiwasu", "❄️ 睦月 Mutsuki", "❄️ 如月 Kisaragi","❄️ 弥生 Yayoi" , "🌸 卯月 Uzuki", "🌸 皐月 Satsuki", "🌸 水無月 Minazuki", 
     ]
 
-    total_irl_days_in_year = 28
+    total_irl_days_in_year = 56
     irl_day_in_year = (irl_day_number - 1) % total_irl_days_in_year + 1
 
-    month_index = (
-        int(math.floor((irl_day_in_year - 1) * 2 / 7))) % len(months)
-    day_index = (irl_day_in_year-1) % 7
-    if day_index == 0:
-        return f"1 - 8 {months[month_index]}"
-    elif day_index == 1:
-        return f"9 - 16 {months[month_index]}"
-    elif day_index == 2:
-        return f"17 - 24 {months[month_index]}"
-    elif day_index == 3:
-        return f"25 {months[month_index]} - 4 {months[month_index+1]}"
-    elif day_index == 4:
-        return f"5 - 12 {months[month_index]}"
-    elif day_index == 5:
-        return f"13 - 20 {months[month_index]}"
-    elif day_index == 6:
-        return f"21 - 28 {months[month_index]}"
-    elif day_index == 7:
-        return f"1 - 8 {months[month_index]}"
+    month_index = (int(math.floor((irl_day_in_year - 1) * 3 / 14))) % len(months)
+    match (irl_day_in_year-1) % 14:
+        case 0:
+            return f"1 - 6 {months[month_index]}"
+        case 1: 
+            return f"7 - 12 {months[month_index]}"
+        case 2: 
+            return f"13 - 18 {months[month_index]}"
+        case 3: 
+            return f"19 - 24 {months[month_index]}"
+        case 4: 
+            return f"25 {months[month_index]} - 3 {months[month_index+1]}"
+        case 5: 
+            return f"4 - 9 {months[month_index]}"
+        case 6: 
+            return f"10 - 15 {months[month_index]}"
+        case 7:
+            return f"16 - 21 {months[month_index]}"
+        case 8:
+            return f"22 - 27 {months[month_index]}"
+        case 9:
+            return f"28 {months[month_index]} - 3 {months[month_index+1]}"
+        case 10:
+            return f"4 - 9 {months[month_index]}"
+        case 11:
+            return f"10 - 15 {months[month_index]}"
+        case 12:
+            return f"16 - 21 {months[month_index]}"
+        case 13:
+            return f"22 - 27 {months[month_index]}"
     raise ValueError("Invalid week number computation.")
 
 
